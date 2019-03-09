e38f582ca2c11318fee43c015762a416d3c2b851
Michael Yoshimura <miyo6032@colorado.edu>
2019-03-09 09:09:57 -0700

    Thread params

--- multi-lookup.c
+++ multi-lookup.c
@@ -5,14 +5,54 @@
 
 struct requester_info
 {
-	const char ** input_file_names;
+	FILE ** files;
 	int file_num;
 };
 
 void * requester(void * ptr)
 {
-	printf("Ima thread.");
+	struct requester_info * info = ((struct requester_info *)ptr);
 	pthread_exit(0);
+}
+
+struct requester_info * init_requester_info(int num_requesters, int num_files, FILE ** files)
+{
+	struct requester_info * params_list = (struct requester_info *)malloc(sizeof(* params_list) * num_requesters);
+
+	for(int i = 0; i < num_requesters; i++)
+	{
+		/*
+		* Initialize parameters for the requester threads
+		*/
+		params_list[i].files = files;
+		params_list[i].file_num = i % num_files;
+	}
+
+	return params_list;
+}
+
+FILE ** open_files(int num_files, const char ** input_file_names)
+{
+	FILE ** files = malloc(sizeof(files) * num_files);
+	for(int i = 0; i < num_files; i++)
+	{
+		FILE * file = fopen(input_file_names[i], "r");
+		if(file == NULL)
+		{
+			printf("File %s could not be loaded.", input_file_names[i]);
+			return NULL;
+		}
+		files[i] = file;
+	}
+	return files;
+}
+
+void close_files(int num_files, FILE ** files)
+{
+	for(int i = 0; i < num_files; i++)
+	{
+		fclose(files[i]);
+	}
 }
 
 int main(int argc, char const *argv[])
@@ -24,6 +64,10 @@ int main(int argc, char const *argv[])
 	const char * requester_log_name;
 	const char * resolver_log_name;
 	int num_files;
+	const char ** input_file_names; 
+	FILE ** files; // The files that are opened for the duration of the program
+	pthread_t * requester_threads; // The requester_threads
+	struct requester_info * requester_params_list; // The requester parameters
 
 	if(argc < minimum_args || argc > maximum_args)
 	{
@@ -37,11 +81,22 @@ int main(int argc, char const *argv[])
 	resolver_log_name = argv[4];
 	num_files = argc - 5;
 
-	const char ** input_file_names = (const char **)malloc(sizeof(char *) * num_files);
-
+	/*
+	* Get the file names from command arguments
+	*/
+	input_file_names = (const char **)malloc(sizeof(char *) * num_files);
 	for(int i = 5; i < argc; i++)
 	{
 		input_file_names[i - 5] = argv[i];
+	}
+
+	/*
+	* Open and validate files
+	*/
+	files = open_files(num_files, input_file_names);
+	if(files == NULL)
+	{
+		return -1;
 	}
 
 	if(num_resolvers > 10 || num_resolvers < 1 || num_requesters > 5 || num_requesters < 1)
@@ -63,22 +118,26 @@ int main(int argc, char const *argv[])
 	* Input Gathering ends here
 	*/
 
-	// Next goal: multiple requester threads reading the files and writing it
-	pthread_t * threads = (pthread_t *)malloc(sizeof(pthread_t) * num_requesters);
+	// Next goal: multiple requester_threads reading the files and writing it
+	requester_threads = (pthread_t *)malloc(sizeof(pthread_t) * num_requesters);
+	requester_params_list = init_requester_info(num_requesters, num_files, files);
 
 	for(int i = 0; i < num_requesters; i++)
 	{
-		pthread_create(&threads[i], NULL, requester, NULL);
+		pthread_create(&requester_threads[i], NULL, requester, &requester_params_list[i]);
 	}
 
 	for(int i = 0; i < num_requesters; i++)
 	{
-		pthread_join(threads[i], NULL);
+		pthread_join(requester_threads[i], NULL);
 	}
 
 	printf("joined!");
 
-	free(threads);
+	free(requester_params_list);
+	free(requester_threads);
+	close_files(num_files, files);
+	free(files);
 	free(input_file_names);
 
 	return 0;
